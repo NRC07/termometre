94a2d7abe81cc78ff8d69a38e7add9e2d8d52251
NRC07 <99951211+NRC07@users.noreply.github.com>
2022-03-07 13:26:29 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,15 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.temperature())
-    basic.showString("Celsius")
-})
-input.onButtonPressed(Button.AB, function () {
-    if (input.temperature() < 25) {
-        basic.showString("Te vas a congelar")
-    } else {
-        basic.showString("Sin camiseta ")
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    if (input.temperature() < 25) {
-        basic.showIcon(IconNames.Sad)
-    } else {
-        basic.showIcon(IconNames.Happy)
-    }
+	
 })
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(input.temperature())
+        basic.showString("Celsius")
+    } else if (input.buttonIsPressed(Button.B)) {
+        if (input.temperature() < 25) {
+            basic.showIcon(IconNames.Sad)
+        } else {
+            basic.showIcon(IconNames.Happy)
+        }
+    }
 })
